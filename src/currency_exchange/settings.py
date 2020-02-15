90d9a3c1d5da2966676813238d8c7d0d3c9ff31b
kravchenko89 <symkin2007@gmail.com>
2020-02-15 18:54:09 +0200

    Login and Logout

--- src/currency_exchange/settings.py
+++ src/currency_exchange/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'currency_exchange.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'account')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,6 +128,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'account.User'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 try:
     from currency_exchange.settings_local import *  # noqa
--- src/currency_exchange/urls.py
+++ src/currency_exchange/urls.py
@@ -1,12 +1,14 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls')),
 
     path('account/', include('account.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
 
 if settings.DEBUG:
