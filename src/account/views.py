94d29233fa899c75624b9bbc835fec891d680127
kravchenko89 <symkin2007@gmail.com>
2020-03-01 01:45:51 +0200

    Readmi

--- src/account/views.py
+++ src/account/views.py
@@ -5,13 +5,7 @@ from django.views import generic
 from django.views.generic import TemplateView
 
 from account.forms import UserCreationForm
-
-
-def smoke(request):
-    # use only with 8001 port
-    # breakpoint()
-    return HttpResponse('smoke')
-
+from account.models import User
 
 # class AboutView(TemplateView):
 #     template_name = '...'
@@ -27,3 +21,10 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+class UserCreate(generic.CreateView):
+    model = User
+    fields = ['username', 'email']
+    template_name = 'registration/registration.html'
+    pass
