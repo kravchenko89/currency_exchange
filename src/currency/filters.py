7ed7433e107465bc6b2ef057f90fed2a6bb5211d
kravchenko89 <symkin2007@gmail.com>
2020-04-08 19:20:41 +0300

    pagin_cw

---
+++ src/currency/filters.py
@@ -0,0 +1,27 @@
+import django_filters
+from django.forms import DateInput
+
+from currency.models import Rate
+
+
+class RateFilter(django_filters.FilterSet):
+    created_date = django_filters.DateFilter(
+        field_name='created',
+        lookup_expr='date',
+        widget=DateInput(
+            attrs={
+                'class': 'datepicker',
+                'type': 'date',
+            }
+        ))
+    # created_dt = django_filters.DateTimeFilter(
+    #     field_name='created',
+    #     widget=DateTimeInput(
+    #         attrs={
+    #             'class': 'datetimepicker',
+    #             'type': 'datetime',
+    #         }
+    #     ))
+    class Meta:
+        model = Rate
+        fields = ['buy', 'sale', 'source', 'created', ]
--- src/currency/views.py
+++ src/currency/views.py
@@ -1,19 +1,40 @@
 import csv
 
 from django.http import HttpResponse
-from django.views.generic import View, ListView
+from django.views.generic import View
 from django.urls import reverse_lazy
+from django_filters.views import FilterView
 
+from currency.filters import RateFilter
 from currency.models import Rate
 
 
-class LatestRates(ListView):
-    model = Rate
+class LatestRates(FilterView):
+    filterset_class = RateFilter
+    queryset = Rate.objects.all()
     template_name = 'rate.html'
-    paginate_by = 20
+    paginate_by = 10
     ordering = ['-created']
     context_object_name = 'rates'
-    success_url = reverse_lazy('index')
+
+    def get_context_data(self, *args, **kwargs):
+        from urllib.parse import urlencode
+        context = super().get_context_data(*args, **kwargs)
+
+        query_params = dict(self.request.GET.items())
+        if 'page' in query_params:
+            del query_params['page']
+        context['query_params'] = urlencode(query_params)
+
+        return context
+
+    # def get_paginate_by(self, queryset):
+    #     super().get_paginate_by()
+
+    # @property
+    # def paginate_by(self):
+    #     paginate = self.request.GET.get('paginate-by')
+    #     return paginate
 
 
 # Create your views here
